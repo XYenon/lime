27e68c0188048bacd5eff2bd59a28fba77030798
xushengfeng <xushengfeng_zg@163.com>
2025-12-13 17:20:36 +0800

    输入 改成post请求

--- rime/lua/llm_pinyin.lua
+++ rime/lua/llm_pinyin.lua
@@ -6,7 +6,8 @@ local ltn12 = require("ltn12")
 local key = "你的密钥"
 
 local headers = {
-  Authorization = 'Bearer ' .. key
+  Authorization = 'Bearer ' .. key,
+  ['content-Type'] = 'application/json'
 }
 
 local base_url = "http://127.0.0.1:5000"
@@ -28,6 +29,11 @@ local function fetch_text(url, op)
     end
   end
 
+  local source = request_op['source']
+  if source then
+    request_op['source'] = ltn12.source.string(source)
+  end
+
   local _, code = http.request(request_op)
 
   -- 将response_body数组转换为字符串
@@ -44,8 +50,14 @@ function translator.init(env)
   env.notifier = env.engine.context.commit_notifier:connect(function(ctx)
     local commit = ctx.commit_history:back()
     if commit then
-      fetch_text(base_url .. "/commit?update=true&new=true&text=" .. url.escape(commit.text), {
-        headers = headers
+      fetch_text(base_url .. "/commit", {
+        headers = headers,
+        method = "post",
+        source = json.encode({
+          text = commit.text,
+          update = true,
+          new = true
+        })
       })
     end
   end)
@@ -67,15 +79,25 @@ function translator.func(input, seg, env)
   if preedit ~= '' then
     local had_select_text = string.sub(preedit, 0, string.len(preedit) - (seg._end - seg.start))
     if had_select_text ~= '' then
-      fetch_text(base_url .. "/commit?update=true&new=false&text=" .. url.escape(had_select_text), {
-        headers = headers
+      fetch_text(base_url .. "/commit", {
+        headers = headers,
+        method = "post",
+        source = json.encode({
+          text = had_select_text,
+          update = true,
+          new = false
+        })
       })
     end
   end
 
   local qp = input
-  local code, reply = fetch_text(base_url .. "/candidates?keys=" .. url.escape(qp), {
-    headers = headers
+  local code, reply = fetch_text(base_url .. "/candidates", {
+    headers = headers,
+    method = "post",
+    source = json.encode({
+      keys = qp
+    })
   })
   local _, j = pcall(json.decode, reply)
   if code == 200 and _ then
